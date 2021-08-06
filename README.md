61f3a35a82e6b863dc8cb94ceec7f21308644786
Alex Gill <alex.gill@theuniprogroup.com>
2021-08-06 23:35:24 +0100

    New features - version 1.1.0
    - Adds JavaScript support to task manager
    - Updates index.html to use iframe with OutSystems local development environment (dependency on requestly chrome extension)
    - Updates README.md (includes instructions on requestly usage)

--- README.md
+++ README.md
@@ -1,4 +1,30 @@
+#FORKED
+##Original version [mediaweb-pt](https://github.com/mediaweb-pt/outsystems-ui-scss)
+
+![version](https://img.shields.io/badge/version-1.1.0-blue)
+
+### New Features
+
+- Index file updated to display OutSystems local development in iframe. This combined with Requestly Chrome Extension allows us to develop front end of application outside of OutSystems platform
+- Support JavaScript (@todo split snippets into seperate files or don't minify to suit OutSystems development)
+  - Babel Transpiler (ES6 to ES5) - OutSystems currently doesnt support ES6 plus browser support varies
+  - Minify (probably redundant for OutSystems)
+  - Eslint (stand config)
+
+####Usage
+
+- To get task manager up and running, please [See below](https://github.com/mediaweb-pt/outsystems-ui-scss#how-to-install)
+- Install Chrome Extension [Requestly](https://chrome.google.com/webstore/detail/requestly-redirect-url-mo/mdnleldcmiljblolnjhpnblkcekpdkpa?hl=en)
+  - Create a new rule
+    - If request: URL: Contains "https://<username>.outsystemscloud.com"
+    - Code Type: CSS
+    - Code Source: URL
+  - Source: http://localhost:3000/dist/outsystems-ui.css
+
+---
+
 # OutSystems UI SCSS
+
 Outsystems UI Reactive version converted to scss
 
 Use this project to create customizations of OutSystems UI using SCSS.
@@ -7,12 +33,12 @@ This will increase your development speed and reduce the maintenance costs overt
 ## How to install
 
 Clone or download the project and run:
+
 - npm install
 
 ## How to Run
 
 - gulp
-
 
 ## 7-1 Architecture
 
@@ -35,7 +61,6 @@ Our SCSS folder follows the 7-1 Architecture pattern.
     |-7-vendors
     |
     `-main.sccs
-    
 
 ## How to install via NPM
 
@@ -47,6 +72,6 @@ $ npm i outsystems-ui-scss
 
 _(Find more in: https://www.npmjs.com/package/outsystems-ui-scss )_
 
-
 # More About the project
+
 https://mediaweb.pt/news/read/outsystems-ui-scss
--- gulpfile.js
+++ gulpfile.js
@@ -1,83 +1,74 @@
-// Load plugins
+'use strict'
 
-const browsersync = require("browser-sync").create();
+const browsersync = require('browser-sync').create();
 const gulp = require('gulp');
 const sass = require('gulp-sass');
-const del = require("del");
+const del = require('del');
 const autoprefixer = require('gulp-autoprefixer');
 const sourcemaps = require('gulp-sourcemaps');
-
-
-// BrowserSync
+const rename = require('gulp-rename')
+const eslint = require('gulp-eslint7')
+const babel = require('gulp-babel')
+const minify = require('gulp-minify')
 
 function browserSync(done) {
-    browsersync.init({
-      server: {
-        baseDir: "./"
-      },
-      port: 3000,
-      startPath: './index.html'
-    });
-    done();
-  }
-
-// BrowserSync reload
-
-function browserSyncReload(done) {
-    browsersync.reload();
-    done();
-  }
-
-
-// Clean vendor
-function clean() {
-    return del(["./vendor/", "./css/"]);
-  }
-
-
-
-// SCSS to CSS compilator
-
-function style(){
-  
-    // 1. Define the input file (scss)
-    return gulp.src('./scss/*.scss')
-
-    // 2. Pass it through sass compiler with map
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    
-    // 3. Add auto prefixes
-    .pipe(autoprefixer({
-        browserslistrc: ['last 3 versions', 'ie >= 10', '> 5%'],
-        cascade: false
-      })
-    )
-    
-    // 4. Save the compiled CSS
-    .pipe(sourcemaps.write('./maps'))
-    .pipe(
-      gulp.dest('./css')
-    )
+  browsersync.init({
+    server: {
+      baseDir: "./"
+    },
+    port: 3000,
+    startPath: './index.html'
+  });
+  done();
 }
 
-// Watching the changes on files
+function browserSyncReload(done) {
+  browsersync.reload();
+  done();
+}
+
+function clean() {
+  return del(['./vendor/', './dist/']);
+}
+
+function styles() {
+  return gulp.src('./scss/*.scss')
+  .pipe(sourcemaps.init())
+  .pipe(sass().on('error', sass.logError))
+  .pipe(autoprefixer({
+      browserslistrc: ['last 3 versions', 'ie >= 10', '> 5%'],
+      cascade: false
+    })
+  )
+  .pipe(sourcemaps.write('./maps'))
+  .pipe(
+    gulp.dest('./dist')
+  )
+}
+
+function scripts() {
+  return gulp.src('./js/*.js')
+    .pipe(eslint())
+    .pipe(babel({ presets: ['@babel/env'] }))
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError())
+    .pipe(minify({ ext: { min: '.min.js' }, noSource: true }))
+    .pipe(gulp.dest('./dist'))
+}
 
 function watchFiles() {
-    gulp.watch("./**/*.css", browserSyncReload);
-    gulp.watch("./**/*.html", browserSyncReload);
-    gulp.watch('./scss/',style);
-  }
-  
+  gulp.watch('./**/*.css', browserSyncReload);
+  gulp.watch('./**/*.html', browserSyncReload);
+  gulp.watch('./**/*.js', browserSyncReload);
+  gulp.watch('./scss/', styles);
+}
 
-// Defining the tasks
 const vendor = gulp.series(clean);
 const build = gulp.series(vendor);
-const watch = gulp.series(build ,gulp.parallel(watchFiles, browserSync,style));
+const watch = gulp.series(build, gulp.parallel(watchFiles, browserSync, styles, scripts));
 
-// Exporting the tasks 
-
-exports.style = style;
+exports.styles = styles;
+exports.scripts = scripts;
 exports.clean = clean;
 exports.vendor = vendor;
 exports.build = build;
